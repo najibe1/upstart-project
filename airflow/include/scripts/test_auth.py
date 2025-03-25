1b7e1750490316b43ec9dac2354172dfd1223644
Eduardo Najibe <eduardo.najibe@ebanx.com>
2025-03-25 16:08:16 -0300

    refactor: airflow and dbt ambients

--- airflow/include/scripts/test_auth.py
+++
@@ -1,5 +0,0 @@
-from google.cloud import storage
-
-client = storage.Client.from_service_account_json('/opt/spark/gcs/keyfile.json')
-buckets = list(client.list_buckets())
-print("Buckets acessíveis:", [b.name for b in buckets])
